b91a863c867dd915b63edf133dab7b08fa619df5
Govardhan Katta <83834179+KattaGovardhan@users.noreply.github.com>
2025-12-17 21:37:17 +0530

    Refactor API base URL configuration

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
-export const url = axios.create({
-  baseURL: baseUrl,
+export const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
 });
